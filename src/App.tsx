d7f19633fac9baff4a0b371c00bdf8438ce5d9db
Dustin Masters <dustin.masters@olo.com>
2020-01-28 12:31:44 -0500

    Add /createUser endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,47 @@ import React from 'react';
 import styles from './App.module.css';
 
 function App() {
-	let [users, setUsers] = React.useState([]);
-	React.useEffect(() => {
-		fetch('/api/hello')
-			.then(x => x.json())
-			.then(x => {
-				setUsers(x.users);
-			});
-	}, []);
-	return (
-		<div className="flex mb-4">
-			<div className="w-full">
-				<div
-					className="max-w-md bg-white shadow-xl border border-green-400 text-green-700 px-4 py-3 rounded relative flex items-center"
-					role="alert"
-				>
-					<strong className="m-1 flex-none font-bold">Hello!</strong>
-					<span className="m-1 flex-1 block sm:inline">
-						This is a test with Tailwind
-					</span>
-					<span className="m-1 flex-1 block sm:inline" data-testid="users">
-						Users: {users.length}
-					</span>
-					<button className={`m-1 flex-none ${styles.btn} ${styles.btnGreen}`}>
-						OK
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+  let [users, setUsers] = React.useState([]);
+  React.useEffect(() => {
+    fetch('/api/users')
+      .then(x => x.json())
+      .then(x => {
+        setUsers(x.users);
+      });
+  }, []);
+
+  let createUser = () => {
+    fetch('/api/createUser', { method: 'POST' })
+      .then(x => fetch('/api/users'))
+      .then(x => x.json())
+      .then(x => {
+        setUsers(x.users);
+      });
+  };
+  return (
+    <div className="flex mb-4">
+      <div className="w-full">
+        <div
+          className="max-w-md bg-white shadow-xl border border-green-400 text-green-700 px-4 py-3 rounded relative flex items-center"
+          role="alert"
+        >
+          <strong className="m-1 flex-none font-bold">Hello!</strong>
+          <span className="m-1 flex-1 block sm:inline">
+            This is a test with Tailwind
+          </span>
+          <span className="m-1 flex-1 block sm:inline" data-testid="users">
+            Users: {JSON.stringify(users)}
+          </span>
+          <button
+            onClick={createUser}
+            className={`m-1 flex-none ${styles.btn} ${styles.btnGreen}`}
+          >
+            OK
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/mirage-setup.js
+++ src/mirage-setup.js
@@ -1,20 +1,23 @@
 import { Server, Model } from 'miragejs';
 
 export function makeServer() {
-	let server = new Server({
-		environment: 'development',
-		models: { user: Model },
-		seeds(server) {
-      server.create("user", { name: "Bob" })
-      server.create("user", { name: "Alice" })
+  let server = new Server({
+    environment: 'development',
+    models: { user: Model },
+    seeds(server) {
+      server.create('user', { name: 'Bob' });
+      server.create('user', { name: 'Alice' });
     },
-		routes() {
-			this.namespace = 'api';
-			this.get('/hello', schema => {
-				return schema.users.all();
-			});
-		}
-	});
+    routes() {
+      this.namespace = 'api';
+      this.get('/users', schema => {
+        return schema.users.all();
+	  });
+	  this.post('/createUser', schema => {
+		  schema.users.create({ name: 'Tester!' });
+	  })
+    }
+  });
 
-	return server;
+  return server;
 }
